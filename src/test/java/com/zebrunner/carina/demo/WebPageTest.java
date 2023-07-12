a1adbd5e8342468ead83759ea5c584ae4b927602
NoleyNarbar <nolan.geiger@gmail.com>
2023-07-12 12:13:02 -0400

    Attempting, still, to test a webpage.

--- src/test/java/com/zebrunner/carina/demo/WebPageTest.java
+++ src/test/java/com/zebrunner/carina/demo/WebPageTest.java
@@ -1,7 +1,8 @@
 package com.zebrunner.carina.demo;
 
 import com.qaprosoft.carina.core.foundation.utils.R;
-import com.qaprosoft.carina.core.foundation.webdriver.DriverHelper;
+import com.qaprosoft.carina.core.foundation.webdriver.DriverFactory;
+import com.qaprosoft.carina.core.foundation.webdriver.IDriverPool;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
@@ -10,13 +11,11 @@ import org.testng.annotations.Test;
 public class WebPageTest {
     @Test
     public void testWebPage() {
-        DriverHelper driverHelper = new DriverHelper();
-
         // Open the webpage
-        driverHelper.getDriver().get(R.TESTDATA.get("https://chat.openai.com/c/fe569920-1559-4cc0-b2bb-7e7653645d7c"));
+        DriverFactory.getDriver().get(R.TESTDATA.get("https://chat.openai.com/c/fe569920-1559-4cc0-b2bb-7e7653645d7c"));
 
         // Find the element containing the expected text
-        WebElement element = driverHelper.getDriver().findElement(By.xpath("//*[text()='Welcome to Carina Demo']"));
+        WebElement element = DriverFactory.getDriver().findElement(By.xpath("//*[text()='Welcome to Carina Demo']"));
 
         // Verify if the element is displayed
         Assert.assertTrue(element.isDisplayed(), "Webpage does not contain the expected text.");
